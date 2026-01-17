48ab877f7d4f5be7334be1df7cafb51ab2478f48
Danushka-Senevirathne <danush15100@gmail.com>
2026-01-17 23:12:16 +0530

    initial commit
    uploaded folder structure

---
+++ siap_backend/src/main/java/com/siap/siap_backend/SiapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siap.siap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiapBackendApplication.class, args);
+	}
+
+}
